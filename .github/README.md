99c0b552d4412199577733993cdd50a56a472ba1
joenguyen0109 <joenguye0109@gmail.com>
2023-04-04 20:59:16 +1000

    first commit

--- .github/README.md
+++ .github/README.md
@@ -29,7 +29,8 @@ You can also just clone this repository directly if you do not want to track you
 git clone https://github.com/<your_user>/<your_repository> ~/.config/nvim/lua/user
 ```
 
-#### Start Neovim
+#### Install package
+sudo apt-get install ripgrep
 
 ```shell
 nvim
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,5 @@
+-- TODO test
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +11,9 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {},
+    event = "VeryLazy",
+  },
 }
